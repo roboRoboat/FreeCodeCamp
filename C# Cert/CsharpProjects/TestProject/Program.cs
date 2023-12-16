238eb642aeebb426366024ac7a671e4a0ed64a67
RotorHead16 <drakec33@gmail.com>
2023-12-15 18:41:58 -0700

    simple logic

--- C# Cert/CsharpProjects/TestProject/Program.cs
+++ C# Cert/CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,31 @@
-﻿Random dice = new();
-int roll = dice.Next();
-Console.WriteLine(roll);
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+string message;
+
+if (daysUntilExpiration == 0)
+{
+    message = "Your subscription has expired.";
+}
+else if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    message = $"Your subscription expires within a day!\nRenew now and save {discountPercentage}%";
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    message = $"Your subscription expires soon!\nRenew now and save {discountPercentage}%";
+}
+else if (daysUntilExpiration <= 10)
+{
+    message = "Your subscription will expire soon. Renew now!";
+}
+else
+{
+    message = "";
+}
+
+Console.WriteLine(daysUntilExpiration);
+Console.WriteLine(message);
--- C# Cert/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ C# Cert/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30e893b518aa09a90c837cfad6f6143e831d6879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6be2aaee560c115208a58006c0667699525877")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
