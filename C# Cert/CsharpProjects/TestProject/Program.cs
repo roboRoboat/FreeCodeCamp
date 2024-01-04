454c55b2723782be50c3a4d2580b2d1f30917935
RotorHead16 <drakec33@gmail.com>
2024-01-04 15:32:01 -0700

    module 03/01

--- C# Cert/CsharpProjects/TestProject/Program.cs
+++ C# Cert/CsharpProjects/TestProject/Program.cs
@@ -1,15 +1,27 @@
-﻿// Console.WriteLine("a" != "a");
-// Console.WriteLine("a" != "A");
-// Console.WriteLine(1 != 2);
-// string myValue = "a";
-// Console.WriteLine(myValue != "a");
+﻿string permission = "Admin|Manager";
+int level = 65;
+string adminTitle = (permission.Contains("Admin") && level > 55) ? "Super Admin" : "Admin";
+string adminGreeting = $"Welcome, {adminTitle} user";
+string managerGreeting = "Contact an Admin for access";
+string accessDenied = "You do not have sufficient privileges";
 
-// Console.WriteLine(1 > 2);
-// Console.WriteLine(1 < 2);
-// Console.WriteLine(1 >= 1);
-// Console.WriteLine(1 <= 1);
-
-string pangram = "The quick brown fox jumped over the lazy dog.";
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
+if (permission.Contains("Admin"))
+{
+    Console.WriteLine(adminGreeting);
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine(managerGreeting);
+    }
+    else
+    {
+        Console.WriteLine(accessDenied);
+    }
+}
+else
+{
+    Console.WriteLine(accessDenied);
+}
 
--- C# Cert/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ C# Cert/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080289bd55af1a2cab9fd21fee1432a9507b6dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78e6cecfdbef8399513207f1cc09c56033ff6c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
